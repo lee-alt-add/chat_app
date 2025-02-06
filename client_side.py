6de9d1212783962e05cb7d2ec9768297122beef1
Lindokuhle Hlatshwayo <lihlatsjhb024@student.wethinkcode.co.za>
2025-02-06 15:25:14 +0200

    Implement client-side functionality to start the chat client and send messages

--- client_side.py
+++ client_side.py
@@ -14,4 +14,21 @@ def receive_messages(client_socket):
             print("Disconnected from the server.")
             client_socket.close()
             break
-        
+
+def start_client():
+    """Start the chat client."""
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect(('127.0.0.1', 5555))  # Replace with server IP
+
+    # Receive the prompt for the name
+    name_prompt = client.recv(1024).decode('utf-8')
+    name = input(name_prompt)
+    client.send(name.encode('utf-8'))
+
+    # Start a thread to receive messages
+    threading.Thread(target=receive_messages, args=(client,)).start()
+
+    # Send messages to the server
+    while True:
+        message = input()
+        client.send(message.encode('utf-8'))
